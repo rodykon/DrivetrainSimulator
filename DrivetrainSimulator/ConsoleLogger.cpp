8b92c37942c7ec3da9570d2a3e7a75fab3e4b426
Rody Konforty <nimrodkonforty@gmail.com>
2019-12-14 23:31:19 +0200

    Marked log() as const

--- DrivetrainSimulator/ConsoleLogger.cpp
+++ DrivetrainSimulator/ConsoleLogger.cpp
@@ -1,6 +1,6 @@
 #include "ConsoleLogger.h"
 
-void ConsoleLogger::log(LogSeverity severity, const std::string& message)
+void ConsoleLogger::log(LogSeverity severity, const std::string& message) const
 {
 	if (severity >= minimumSeverity) {
 		std::cout << severity << ": " << message.data() << std::endl;
--- DrivetrainSimulator/ConsoleLogger.h
+++ DrivetrainSimulator/ConsoleLogger.h
@@ -8,7 +8,7 @@ public:
 	ConsoleLogger(LogSeverity minimumSeverity) : minimumSeverity(minimumSeverity) {}
 	virtual ~ConsoleLogger() = default;
 
-	virtual void log(LogSeverity severity, const std::string& message) override;
+	virtual void log(LogSeverity severity, const std::string& message) const override;
 	
 	virtual void setMinimumSeverity(LogSeverity severity) override;
 
